860a5cef6fe82a59bc4d79b2d49ac030d01699fb
AbdElrhman ElHodaky <abdo.arh38@yahoo.com>
2024-10-08 16:22:30 +0300

    Update footer.blade.php

--- resources/views/users/layout/footer.blade.php
+++ resources/views/users/layout/footer.blade.php
@@ -35,9 +35,9 @@
             <div class="col-lg-3 col-sm-3">
                 <h4>Contact us</h4>
                 <p><b>Bootstrap Realestate Inc.</b><br>
-                    <span class="glyphicon glyphicon-map-marker"></span>8290 Walk Street, Australia<br>
+                 <!--   <span class="glyphicon glyphicon-map-marker"></span>8290 Walk Street, Australia<br>-->
                     <span class="glyphicon glyphicon-envelope"></span>hello@bootstrapreal.com<br>
-                    <span class="glyphicon glyphicon-earphone"></span>(123) 456-7890
+                  <!--  <span class="glyphicon glyphicon-earphone"></span>(123) 456-7890 -->
                 </p>
             </div>
         </div>
